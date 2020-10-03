4f9d0f22f4c6598de814df1380e32465aa2f56f3
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-03 19:41:36 +0530

    [Praveen] Add: After Rolling the Dice getting the option for NoPlay, Snake or Ladder

--- Program.cs
+++ Program.cs
@@ -34,6 +34,25 @@ namespace SnakeAndLadderProblem
                     Console.WriteLine("Current Moves Number={0}", moves);
                     int diceInput = dice.Next(1, 7);
                     Console.WriteLine("Your Dice Score={0}", diceInput);
+                    playerPosition += diceInput;
+                    moves++;
+                    int opPlay = option.Next(0, 3);
+                    switch (opPlay)
+                    {
+                        case 0:
+                            Console.WriteLine("Your Option is No Play.");
+                            break;
+                        case 1:
+                            Console.WriteLine("Your Option is Ladder.");
+                            playerPosition += diceInput;
+                            break;
+                        case 2:
+                            Console.WriteLine("Your Option is Snakes.");
+                            playerPosition -= diceInput;
+                            break;
+                    }
+                    Console.WriteLine("Your Position={0}", playerPosition);
+                    Console.WriteLine("Your Moves={0}", moves);
 
                     break;
 
