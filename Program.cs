72e745c180a632ff99483d23c5fe3d0cc08a22e5
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-03 20:55:51 +0530

    [Praveen] Add: Added the Dual Player Mode

--- Program.cs
+++ Program.cs
@@ -51,7 +51,7 @@ namespace SnakeAndLadderProblem
                         {
                             positionPlayer += diceInput;
                         }
-
+                        
                         int optionPlay = option.Next(0, 3);
                         switch (optionPlay)
                         {
@@ -85,6 +85,133 @@ namespace SnakeAndLadderProblem
                         Console.WriteLine("Your Moves={0}", moves);
                     }
                     break;
+
+                case 2:
+                    int positionFirstPlayer = START;
+                    int positionSecondPlayer = START;
+                    int turnPlayer = 1;
+                    int movesFirstPlayer = 0;
+                    int movesSecondPlayer = 0;
+
+                    //Loop to ensure either wining condition
+                    while (positionFirstPlayer != END && positionSecondPlayer != END)
+                    {
+                        if (turnPlayer == 1)
+                        {
+                            
+                            int diceInput = dice.Next(1, 7);
+                            movesFirstPlayer++;
+                            Console.WriteLine("Your Dice Score={0}", diceInput);
+                            if (positionFirstPlayer + diceInput > END)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                positionFirstPlayer += diceInput;
+                            }
+
+                            int optionPlay = option.Next(0, 3);
+                            switch (optionPlay)
+                            {
+                                case 0:
+                                    Console.WriteLine("Your Option is No Play.");
+                                    break;
+                                case 1:
+                                    Console.WriteLine("Your Option is Ladder.");
+                                    if (positionFirstPlayer + diceInput > END)
+                                    {
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        positionFirstPlayer += diceInput;
+                                    }
+                                    turnPlayer = 1;
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Your Option is Snakes.");
+                                    positionFirstPlayer -= diceInput;
+                                    break;
+                            }
+                            if (positionFirstPlayer < 0)
+                            {
+                                positionFirstPlayer = START;
+                            }
+                            Console.WriteLine("First Player Position={0}", positionFirstPlayer);
+                            Console.WriteLine("First Player Moves={0}", movesFirstPlayer);
+                            turnPlayer = 2;
+                        }
+
+                        else if (turnPlayer == 2)
+                        {
+                            int diceInput = dice.Next(1, 7);
+                            movesSecondPlayer++;
+                            Console.WriteLine("Your Dice Score={0}", diceInput);
+                            if (positionSecondPlayer + diceInput > END)
+                            {
+                                continue;
+                            }
+                            else
+                            {
+                                positionSecondPlayer += diceInput;
+                            }
+
+                            int opPlay = option.Next(0, 3);
+                            switch (opPlay)
+                            {
+                                case 0:
+                                    Console.WriteLine("Your Option is No Play.");
+                                    break;
+                                case 1:
+                                    Console.WriteLine("Your Option is Ladder.");
+                                    if (positionSecondPlayer + diceInput > END)
+                                    {
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        positionSecondPlayer += diceInput;
+                                    }
+                                    turnPlayer = 2;
+                                    break;
+                                case 2:
+                                    Console.WriteLine("Your Option is Snakes.");
+                                    positionSecondPlayer -= diceInput;
+                                    break;
+                            }
+                            if (positionSecondPlayer < 0)
+                            {
+                                positionSecondPlayer = START;
+                            }
+                            Console.WriteLine("Second Player Position={0}", positionSecondPlayer);
+                            Console.WriteLine("Second Player Moves={0}", movesSecondPlayer);
+                            turnPlayer = 1;
+                        }
+                    }
+                    if (positionFirstPlayer == END)
+                    {
+                        Console.WriteLine("First Player Position={0}", positionFirstPlayer);
+                        Console.WriteLine("Player-1 is Winner and total moves in game is {0}", movesFirstPlayer + movesSecondPlayer);
+                    }
+
+                    else if (positionSecondPlayer == END)
+                    {
+                        Console.WriteLine("Second Player Position={0}", positionSecondPlayer);
+                        Console.WriteLine("Player-2 is Winner and total moves in game is {0}", movesFirstPlayer + movesSecondPlayer);
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("Player-1={0}  ,  Player-2={1}", positionFirstPlayer, positionSecondPlayer);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Supports only two Modes!!!!!!!!!!");
+                    break;
+
+
 
             }
         }
