f67cdbb6385f496b7a8c275b16fef657aa859f24
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-03 19:18:21 +0530

    [Praveen] Add: Welcome message to the game

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadderProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine("Welcome to Snakes and Ladder Game");
+            Console.WriteLine("=================================");
+        }
+    }
+}
