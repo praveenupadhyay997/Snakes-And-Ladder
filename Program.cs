327252a3ea7bc70aee7005575ff2be6e99fc15a3
praveenupadhyay997 <praveenupadhyay997@gmail.com>
2020-10-03 19:47:34 +0530

    [Praveen] Add: Winning Condition for the Single Player Mode

--- Program.cs
+++ Program.cs
@@ -27,33 +27,41 @@ namespace SnakeAndLadderProblem
             switch (choice)
             {
                 case 1:
-                    int playerPosition = START;
+                    int positionPlayer = START;
                     int moves = 0;
 
-                    Console.WriteLine("Current Position Of Player-1 is {0}", playerPosition);
+                    Console.WriteLine("Current Position Of Player-1 is {0}", positionPlayer);
                     Console.WriteLine("Current Moves Number={0}", moves);
-                    int diceInput = dice.Next(1, 7);
-                    Console.WriteLine("Your Dice Score={0}", diceInput);
-                    playerPosition += diceInput;
-                    moves++;
-                    int opPlay = option.Next(0, 3);
-                    switch (opPlay)
-                    {
-                        case 0:
-                            Console.WriteLine("Your Option is No Play.");
-                            break;
-                        case 1:
-                            Console.WriteLine("Your Option is Ladder.");
-                            playerPosition += diceInput;
-                            break;
-                        case 2:
-                            Console.WriteLine("Your Option is Snakes.");
-                            playerPosition -= diceInput;
-                            break;
-                    }
-                    Console.WriteLine("Your Position={0}", playerPosition);
-                    Console.WriteLine("Your Moves={0}", moves);
 
+                    while (positionPlayer < 100)
+                    {
+
+                        int diceInput = dice.Next(1, 7);
+                        Console.WriteLine("Your Dice Score={0}", diceInput);
+                        positionPlayer += diceInput;
+                        moves++;
+                        int opPlay = option.Next(0, 3);
+                        switch (opPlay)
+                        {
+                            case 0:
+                                Console.WriteLine("Your Option is No Play.");
+                                break;
+                            case 1:
+                                Console.WriteLine("Your Option is Ladder.");
+                                positionPlayer += diceInput;
+                                break;
+                            case 2:
+                                Console.WriteLine("Your Option is Snakes.");
+                                positionPlayer -= diceInput;
+                                break;
+                        }
+                        if (positionPlayer < 0)
+                        {
+                            positionPlayer = START;
+                        }
+                        Console.WriteLine("Your Position={0}", positionPlayer);
+                        Console.WriteLine("Your Moves={0}", moves);
+                    }
                     break;
 
             }
